ea7fbe5a486d97e2bae22353dc04c95166568565
LETICIA BEATRIZ MARTINS <24122309@dsn127m1249388.cfp127aluno.local>
2025-05-13 10:01:09 -0300

    finalizado

--- app/src/main/java/br/senai/sp/jandira/profile/screens/TelaInicio.kt
+++ app/src/main/java/br/senai/sp/jandira/profile/screens/TelaInicio.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -16,9 +18,13 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -27,11 +33,9 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -247,110 +251,265 @@ fun TelaInicio() {
                 modifier = Modifier
                     .fillMaxSize(),
                 verticalArrangement = Arrangement.Top
-            ) {
-                Row (
-                modifier = Modifier
-                    .padding(horizontal = 20.dp)
-                    .padding(15.dp),
-                horizontalArrangement = Arrangement.Absolute.Left
-            ){
-                Icon(
-                    imageVector = Icons.Default.Email,
-                    contentDescription = "",
-                    tint = Color(0xFFD4C7EF),
+            )
+            {
+                Row(
                     modifier = Modifier
-                        .size(22.dp)
+                        .padding(horizontal = 10.dp)
+                        .padding(15.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Email,
+                        contentDescription = " ",
+                        tint = Color(0xFFD4C7EF),
+                        modifier = Modifier.size(22.dp)
+                    )
+                    Text(
+                        text = stringResource(R.string.email),
+                        fontSize = 15.sp,
+                        color = Color(0xFFCBCBEF),
+                        modifier = Modifier.padding(start = 8.dp)
+                    )
+                }
+                Text(
+                    modifier = Modifier
+                        .padding(horizontal = 23.dp),
+                    text = stringResource(R.string.official),
+                    color = Color.LightGray,
+                    fontSize = 11.sp,
                 )
                 Text(
-                    text = stringResource(R.string.email),
-                    fontSize = 15.sp,
-                    color = Color(0xFFEAEBF1),
+                    text = stringResource(R.string.emailFic),
+                    color = Color(0xFF6C5E86),
+                    fontSize = 13.sp,
                     modifier = Modifier
-                        .padding(4.dp)
+                        .padding(
+                            top = 5.dp,
+                            bottom = 8.dp
+                        )
+                        .padding(horizontal = 23.dp),
                 )
-            }
-                Column (
+                Text(
                     modifier = Modifier
-                        .padding(horizontal = 30.dp)
-                        .padding(top = 10.dp)
-                        .padding(bottom = 15.dp)
-                ){
-                    Text(
-                        text = stringResource(R.string.official),
-                        fontSize = 15.sp,
-                        color = Color(0xFFB596F1)
+                        .padding(horizontal = 23.dp),
+                    text = stringResource(R.string.personal),
+                    color = Color.LightGray,
+                    fontSize = 11.sp,
+                )
+                Text(
+                    text = stringResource(R.string.personalFic),
+                    color = Color(0xFF6C5E86),
+                    fontSize = 13.sp,
+                    modifier = Modifier
+                        .padding(
+                            top = 5.dp,
+                            bottom = 8.dp
+                        )
+                        .padding(horizontal = 23.dp),
+                )
+                HorizontalDivider(
+                    modifier = Modifier
+                        .padding(
+                            top = 10.dp,
+                            start = 20.dp,
+                            end = 20.dp
+                        ),
+                    color = Color(0x3CA6A5A3)
+                )
+                Row(
+                    modifier = Modifier
+                        .padding(horizontal = 10.dp)
+                        .padding(15.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Call,
+                        contentDescription = " ",
+                        tint = Color(0xFFD4C7EF),
+                        modifier = Modifier.size(22.dp)
                     )
                     Text(
-                        text = stringResource(R.string.emailFic),
+                        text = stringResource(R.string.phone),
                         fontSize = 15.sp,
+                        color = Color(0xFFCBCBEF),
+                        modifier = Modifier.padding(start = 8.dp)
+                    )
+                }
+                Text(
+                    modifier = Modifier
+                        .padding(horizontal = 23.dp),
+                    text = stringResource(R.string.mobile),
+                    color = Color.LightGray,
+                    fontSize = 11.sp,
+                )
+                Text(
+                    text = stringResource(R.string.number),
+                    color = Color(0xFF6C5E86),
+                    fontSize = 13.sp,
+                    modifier = Modifier
+                        .padding(
+                            top = 5.dp,
+                            bottom = 8.dp
+                        )
+                        .padding(horizontal = 23.dp),
+                )
+                HorizontalDivider(
+                    modifier = Modifier
+                        .padding(
+                            top = 10.dp,
+                            start = 20.dp,
+                            end = 20.dp
+                        ),
+                    color = Color(0x3CA6A5A3)
+                )
+                Row(
+                    modifier = Modifier
+                        .padding(horizontal = 10.dp)
+                        .padding(15.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Person,
+                        contentDescription = " ",
+                        tint = Color(0xFFD4C7EF),
+                        modifier = Modifier.size(22.dp)
+                    )
+                    Text(
+                        text = stringResource(R.string.team),
+                        fontSize = 15.sp,
+                        color = Color(0xFFCBCBEF),
+                        modifier = Modifier.padding(start = 8.dp)
+                    )
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 23.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = stringResource(R.string.teamName),
                         color = Color(0xFF6C5E86),
-                        modifier = Modifier
-                            .padding(top = 4.dp)
+                        fontSize = 13.sp
                     )
-                    Text(
-                        text = stringResource(R.string.personal),
-                        fontSize = 15.sp,
-                        color = Color(0xFFB596F1),
-                        modifier = Modifier
-                            .padding(top =  15.dp)
-                    )
-                    Text(
-                        text = stringResource(R.string.personalFic),
-                        fontSize = 15.sp,
-                        color = Color(0xFF6C5E86),
-                        modifier = Modifier
-                            .padding(top = 4.dp)
-                    )
-                    HorizontalDivider(
-                        modifier = Modifier
-                            .padding(
-                                top = 15.dp,
-                                start = 2.dp,
-                                end = 20.dp
-                            ),
-                        color =
-                        Color(color = 0xFFEAEBF1)
-                    )
-                    Row (
-                        modifier = Modifier
-
-                                .padding(10.dp),
-                        horizontalArrangement = Arrangement.Absolute.Left
-                        ){
+                    Spacer(modifier = Modifier.weight(1f))
+                    Button(
+                        onClick = { },
+                        contentPadding = PaddingValues(0.dp),
+                        modifier = Modifier.size(36.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(0xFFD4C7EF)
+                        )
+                    ) {
                         Icon(
-                                imageVector = Icons.Default.Call,
-                                contentDescription = "",
+                            imageVector = Icons.Filled.ArrowForward,
+                            contentDescription = "",
+                            tint = Color.White
+                        )
+                    }
+                }
+                HorizontalDivider(
+                    modifier = Modifier
+                        .padding(
+                            top = 10.dp,
+                            start = 20.dp,
+                            end = 20.dp
+                        ),
+                    color = Color(0x3CA6A5A3)
+                )
+                Row(
+                    modifier = Modifier
+                        .padding(horizontal = 10.dp)
+                        .padding(15.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Person,
+                        contentDescription = " ",
+                        tint = Color(0xFFD4C7EF),
+                        modifier = Modifier.size(22.dp)
+                    )
+                    Text(
+                        text = stringResource(R.string.leads),
+                        fontSize = 15.sp,
+                        color = Color(0xFFCBCBEF),
+                        modifier = Modifier.padding(start = 8.dp)
+                    )
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 23.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = stringResource(R.string.leadsName),
+                        color = Color(0xFF6C5E86),
+                        fontSize = 13.sp
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                    Button(
+                        onClick = { },
+                        contentPadding = PaddingValues(0.dp),
+                        modifier = Modifier.size(36.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(0xFFD4C7EF)
+                        )
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowForward,
+                            contentDescription = "",
+                            tint = Color.White
+                        )
+                    }
+                }
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 16.dp),
+                    verticalArrangement = Arrangement.Bottom,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Button(
+                            onClick = { },
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = Color.White,
+                                contentColor = Color(0xFFEC829B)
+                            ),
+                            border = BorderStroke(1.dp,
+                                Color(0xFFBDBDBD))
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.AddCircle,
+                                contentDescription = " ",
                                 tint = Color(0xFFD4C7EF),
-                                modifier = Modifier
-                                    .size(22.dp)
+                                modifier = Modifier.size(22.dp)
                             )
-                        Text(
-                                text = stringResource(R.string.phone),
-                                fontSize = 15.sp,
-                                color = Color(0xFFEAEBF1),
-                                modifier = Modifier
-                                    .padding(4.dp)
+                            Text(text = stringResource(R.string.add),
+                                color = Color(0xFF6C5E86),
                             )
                         }
-                    Column (
-                        modifier = Modifier
-                            .padding(horizontal = 30.dp)
-                            .padding(top = 10.dp)
-                            .padding(bottom = 15.dp)
-                    ) {
-                        Text(
-                            text = stringResource(R.string.mobile),
-                            fontSize = 15.sp,
-                            color = Color(0xFFB596F1)
-                        )
-                        Text(
-                            text = stringResource(R.string.number),
-                            fontSize = 15.sp,
-                            color = Color(0xFF6C5E86),
-                            modifier = Modifier
-                                .padding(top = 4.dp)
-                        )
-
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Button(
+                            onClick = { },
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = Color.White,
+                                contentColor = Color(0xFF1F1F1F)
+                            ),
+                            border = BorderStroke(1.dp, Color(0xFFBDBDBD)),
+                            contentPadding = PaddingValues(8.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Share,
+                                contentDescription = "",
+                                tint = Color(0xFF6C5E86),
+                                modifier = Modifier.size(20.dp)
+                            )
+                        }
                     }
                 }
             }
